d36dafbb59fcb93f83dcaf84e51c7c26649abd65
sandra_c <chavezsandra0498@gmail.com>
2020-02-26 01:11:48 -0800

    Update initial commit

--- RemindMe/app/src/main/java/com/example/remindme/MainActivity.kt
+++ RemindMe/app/src/main/java/com/example/remindme/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.remindme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val enterButton = findViewById<Button>(R.id.enterButton)
+
+        //connects the user's entry of an event
+        val editEvent = findViewById<EditText>(R.id.editEvent)
+
+        //connects the user's entry of a specific date
+        val editDate = findViewById<EditText>(R.id.editDate)
+
+
+
     }
 }
