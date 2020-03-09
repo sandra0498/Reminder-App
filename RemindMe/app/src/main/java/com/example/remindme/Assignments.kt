d54b0a626dbb65af32f6dcf61af6eb11075446df
sandra_c <chavezsandra0498@gmail.com>
2020-03-08 19:11:24 -0700

    Updates

---
+++ RemindMe/app/src/main/java/com/example/remindme/Assignments.kt
@@ -0,0 +1,5 @@
+package com.example.remindme
+
+import java.sql.Date
+
+class Assignments(var assignment : String, val dueDate: Date)
--- RemindMe/app/src/main/java/com/example/remindme/MainActivity.kt
+++ RemindMe/app/src/main/java/com/example/remindme/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.sql.Date
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editEvent: EditText
+    lateinit var  enterDate: Date
 
     //creating the class constants
     companion object{
@@ -20,13 +27,14 @@ class MainActivity : AppCompatActivity() {
 
 
         //connects the user's entry of an event
-        val editEvent = findViewById<EditText>(R.id.editEvent)
+        editEvent = findViewById(R.id.editEvent)
 
         //connects the user's entry of a specific date
-        val editDate = findViewById<EditText>(R.id.EnterDate)
+        var editDate = findViewById<EditText>(R.id.EnterDate)
 
         val enterButton = findViewById<Button>(R.id.enterButton)
         enterButton.setOnClickListener {
+            saveData()
 
             val assignment = editEvent.text.toString()
             val dueDate = editDate.text.toString()
@@ -40,5 +48,16 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    private fun saveData(){
+        val assignment = editEvent.text.toString()
+
+        if(assignment.isEmpty()){
+            editEvent.error = "Please enter an assignment! "
+            return
+        }
+
+        val reference = FirebaseDatabase.getInstance().getReference("Assignments")
     }
 }
